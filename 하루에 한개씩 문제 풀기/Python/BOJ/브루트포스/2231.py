801a5c1c1d7c78a2141df6fe820f04d792fcbc1b
hangyuseong <tyty587587@gmail.com>
2021-04-09 17:52:53 +0900

    20201.04.09.금 : 백준, 2231, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/브루트포스/2231.py
@@ -0,0 +1,12 @@
+# boj, 2231 : 분해합, python3
+N = int(input())
+result = 0
+
+for i in range(1, N+1):
+    a = list(map(int, str(i)))
+    s = i + sum(a)
+    if(s == N):
+        result = i
+        break
+
+print(result)
