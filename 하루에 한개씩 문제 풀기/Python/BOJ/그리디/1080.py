f77426438b13fe61639b7a31c0431c13ebb07f41
KYUSEONGHAN <tyty587587@gmail.com>
2022-01-19 15:43:56 +0900

    2022.1.19.수: boj, 14487: 행렬, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/그리디/1080.py
@@ -0,0 +1,33 @@
+# boj, 1080:행렬, python3
+import sys
+
+input = sys.stdin.readline
+
+def othello(matrix, x, y):
+    for i in range(3):
+        for j in range(3):
+            if matrix[x+i][y+j] == '0':
+                matrix[x+i][y+j] = '1'
+            else:
+                matrix[x+i][y+j] = '0'
+
+def solve(n, m, matrix_a, matrix_b):
+    cnt = 0
+
+    for x in range(n-2):
+        for y in range(m-2):
+            if matrix_a[x][y] != matrix_b[x][y]:
+                othello(matrix_a, x, y)
+                cnt += 1
+
+    if matrix_a != matrix_b:
+        return -1
+
+    return cnt
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    matrix_a = [list(map(str, input())) for _ in range(n)]
+    matrix_b = [list(map(str, input())) for _ in range(n)]
+
+    print(solve(n, m, matrix_a, matrix_b))
