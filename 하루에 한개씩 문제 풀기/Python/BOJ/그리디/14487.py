f1bf59a6cebe45e796e96807078cfb7ad4d728b7
KYUSEONGHAN <tyty587587@gmail.com>
2022-01-19 14:29:25 +0900

    2022.1.19.수: boj, 14487: 욱제는 효도쟁이야!!, python3

--- 하루에 한개씩 문제 풀기/Python/BOJ/그리디/14487.py
+++ 하루에 한개씩 문제 풀기/Python/BOJ/그리디/14487.py
@@ -1,10 +1,26 @@
 # boj, 14487: 욱제는 효도쟁이야!!, python3
+
+# 풀이1 -> 정렬을 이용한 풀이
 import sys
 
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 def solve(l):
     return sum(sorted(l)[:-1])
+
+if __name__ == '__main__':
+    n = int(input())
+    cost_list = list(map(int, input().split()))
+
+    print(solve(cost_list))
+
+# 풀이2 -> max 내장함수를 이용한 풀이
+import sys
+
+input = sys.stdin.readline
+
+def solve(l):
+    return sum(l) - max(l)
 
 if __name__ == '__main__':
     n = int(input())
