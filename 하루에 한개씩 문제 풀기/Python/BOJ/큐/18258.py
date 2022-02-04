1f2faae0700d303092fd482059efef7912d2db9e
KYUSEONGHAN <tyty587587@gmail.com>
2022-02-04 17:49:25 +0900

    2022.02.04.금: boj, 18258: 큐 2, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/큐/18258.py
@@ -0,0 +1,57 @@
+# boj, 18258: 큐 2, python3
+from collections import deque
+
+def push(queue, x):
+    queue.appendleft(x)
+
+def pop(queue):
+    if not queue:
+        print(-1)
+    else:
+        print(queue[-1])
+        queue.pop()
+
+def size(queue):
+    print(len(queue))
+
+def empty(queue):
+    if not queue:
+        print(1)
+    else:
+        print(0)
+
+def front(queue):
+    if queue:
+        print(queue[-1])
+    else:
+        print(-1)
+
+def back(queue):
+    if queue:
+        print(queue[0])
+    else:
+        print(-1)
+
+def solve(order_list):
+    queue = deque()
+
+    for x in order_list:
+        if x == 'pop':
+            pop(queue)
+        elif x == 'size':
+            size(queue)
+        elif x == 'empty':
+            empty(queue)
+        elif x == 'front':
+            front(queue)
+        elif x == 'back':
+            back(queue)
+        else:
+            push_num = x.split()[-1]
+            push(queue, push_num)
+
+if __name__ == '__main__':
+    n = int(input())
+    order_list = [str(input()) for _ in range(n)]
+
+    solve(order_list)
