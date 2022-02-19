ebf379af90ee02ebd86a975669c908e9dc2641db
KYUSEONGHAN <tyty587587@gmail.com>
2022-02-19 17:51:22 +0900

    2022.2.19.토: boj, 13301: 타일 장식물, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/다이나믹_프로그래밍/13301.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/13301
+# boj, 13301: 타일 장식물, python3
+import sys
+
+input = sys.stdin.readline
+
+def dp(n):
+    # 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+    d = [0] * 81
+
+    d[1] = 4
+    d[2] = 6
+
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
+
+    return d[n]
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(dp(n))
