aba7fdadd3cabdac201751c530cc6c6458252059
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-08 14:26:43 +0900

    2022.3.8.화: boj, 2161: 카드 1, python3

--- 하루에 한개씩 문제 풀기/Python/BOJ/큐/2161.py
+++ 하루에 한개씩 문제 풀기/Python/BOJ/큐/2161.py
@@ -1,2 +1,27 @@
 # https://www.acmicpc.net/problem/2161
-print('test')
+# boj, 2161: 카드1, python3
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+def solve(n):
+    queue = deque([x for x in range(1, n+1)])
+    cnt = 1
+    result = []
+
+    while queue:
+        if cnt % 2 == 1:
+            x = queue.popleft()
+            result.append(x)
+        else:
+            x = queue.popleft()
+            queue.append(x)
+        cnt += 1
+
+    return result
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(*solve(n))
