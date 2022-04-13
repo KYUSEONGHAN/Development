86ce6c297b107cfb81f2ce9abe4d01ee453bb991
KYUSEONGHAN <tyty587587@gmail.com>
2022-04-13 19:43:53 +0900

    2022.4.13.수: boj, 11718: 그대로 출력하기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/구현/11718.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/11718
+# boj, 11718: 그대로 출력하기, python3
+def solve():
+    while True:
+        try:
+            print(input())
+        except:
+            break
+
+if __name__ == '__main__':
+    solve()
