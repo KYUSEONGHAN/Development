9e42ea6837303f4ccb1e962aa509812b222d5671
KYUSEONGHAN <tyty587587@gmail.com>
2022-04-15 14:21:08 +0900

    2022.4.15.금: sw expert academy, 2046: 스탬프 찍기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/SW Expert Academy/D1/2046. 스탬프 찍기.py
@@ -0,0 +1,9 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKdT6AyYDFAUq&categoryId=AV5QKdT6AyYDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=1&&&&&&&&&&
+# sw expert academy, d1: 2046. 스탬프 찍기, python3
+def solve(n: int) -> str:
+    return '#' * n
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(solve(n))
