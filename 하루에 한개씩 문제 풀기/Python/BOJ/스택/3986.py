efb112e6eee24580262c581232148fe3faa5d607
KYUSEONGHAN <tyty587587@gmail.com>
2022-04-28 22:11:18 +0900

    2022.4.28.목: boj, 3986: 좋은 단어, python3

--- 하루에 한개씩 문제 풀기/Python/BOJ/스택/3986.py
+++ 하루에 한개씩 문제 풀기/Python/BOJ/스택/3986.py
@@ -1,22 +1,47 @@
-# boj, 3986: 좋은 단어, stack, python3
+# 좋은단어인지 체크하는 함수
 def good_word(word):
+    # 스택 자료구조 선언
     stack = []
 
+    # 입력받은 문자를 하나하나 반복문으로 순회
     for x in word:
+        # stack에 값이 없다면
         if not stack:
+            # 스택에 현재 요소 추가
             stack.append(x)
+            # 아래 조건문 수행 안하도록
             continue
+        # 현재 요소가 스택의 최신(top)과 다르다면
         if x != stack[-1]:
+            # 스택에 추가
             stack.append(x)
+        # 현재 요소가 스택의 최신(top)과 같다면
         elif x == stack[-1]:
+            # pop -> 꺼낸다
             stack.pop()
 
-    return 0 if stack else 1
+    # 만약 스택에 값이 있다면 0을 반환
+    if stack:
+        return 0
+    # stack에 값이 없다면 좋은단어이므로 1을 반환
+    else:
+        return 1
 
+# main solution 함수
 def solution(N):
-    return sum([good_word(str(input())) for _ in range(N)])
+    # 좋은단어수를 담는 list
+    result = []
 
-if __name__ == '__main__':
-    N = int(input())
+    # 반복할 변수만큼 반복
+    for _ in range(N):
+        # 좋은단어인지 체크하고 result list에 append (추가)
+        result.append(good_word(str(input())))
 
-    print(solution(N))
+    # 좋은 단어 수 반환
+    return sum(result)
+
+# 반복할 변수 입력받기
+N = int(input())
+
+# 정답 출력
+print(solution(N))
