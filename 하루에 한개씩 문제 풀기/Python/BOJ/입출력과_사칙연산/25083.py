8377d52fbf445e605d2ce7e7afe87209a6418975
KYUSEONGHAN <tyty587587@gmail.com>
2022-05-03 19:50:01 +0900

    2022.5.3.화: boj, 25083: 새싹, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/입출력과_사칙연산/25083.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/25083
+# boj, 25083: 새싹, python3
+def solve():
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
+if __name__ == '__main__':
+    solve()
