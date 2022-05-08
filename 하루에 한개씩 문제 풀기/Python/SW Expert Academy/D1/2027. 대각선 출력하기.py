1e140ce8204571beff384926b0b3d1f755df5fc1
KYUSEONGHAN <tyty587587@gmail.com>
2022-05-08 12:53:13 +0900

    2022.5.8.일: sw expert academy, 2027: 대각선 출력하기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/SW Expert Academy/D1/2027. 대각선 출력하기.py
@@ -0,0 +1,11 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QFuZ6As0DFAUq&categoryId=AV5QFuZ6As0DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=2
+# sw expert academy, d1: 2027. 대각선 출력하기, python3
+def solve():
+    print('#++++')
+    print('+#+++')
+    print('++#++')
+    print('+++#+')
+    print('++++#')
+
+if __name__ == '__main__':
+    solve()
