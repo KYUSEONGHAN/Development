6ae91d2b2780e2d1ecbc8000e4a8a79eb9c1bcfc
KYUSEONGHAN <tyty587587@gmail.com>
2022-05-10 10:29:00 +0900

    2022.5.10.화: boj, 5388: 마이크로소프트 로고, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/구현/5388.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/5338
+# boj, 5388: 마이크로소프트 로고, python3
+def solve():
+    print("       _.-;;-._")
+    print("'-..-'|   ||   |")
+    print("'-..-'|_.-;;-._|")
+    print("'-..-'|   ||   |")
+    print("'-..-'|_.-''-._|")
+
+if __name__ == '__main__':
+    solve()
