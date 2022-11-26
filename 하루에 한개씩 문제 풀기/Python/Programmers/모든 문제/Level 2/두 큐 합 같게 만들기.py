6673b735720d4f2cf2d1d00cf4079dbacae113f6
KYUSEONGHAN <tyty587587@gmail.com>
2022-11-27 00:20:07 +0900

    2022.11.27.일: programmers, level2: 두 큐 합 같게 만들기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 2/두 큐 합 같게 만들기.py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/118667
+# programmers, level0: 두 큐 합 같게 만들기, python3
+from collections import deque
+
+def solution(queue1: list, queue2: list) -> int:
+    queue1, queue2 = deque(queue1), deque(queue2)
+    answer = 0
+
+    for _ in range(len(queue1) * 3):
+        if sum(queue1) > sum(queue2):
+            queue2.append(queue1.popleft())
+        elif sum(queue1) < sum(queue2):
+            queue1.append(queue2.popleft())
+        else:
+            return answer
+        answer += 1
+
+    return -1
+
+if __name__ == '__main__':
+    print(solution([3, 2, 7, 2], [4, 6, 5, 1]))    # 2
+    print(solution([1, 2, 1, 2], [1, 10, 1, 2]))   # 7
+    print(solution([1, 1], [1, 5]))                # -1
