422034ffd402c13cfc25ef913b62f0685b41d13f
KYUSEONGHAN <tyty587587@gmail.com>
2023-01-18 15:05:46 +0900

    2023.1.18.수: programmers, level0: 두 수의 나눗셈, python3

---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 0/두 수의 나눗셈.py
@@ -0,0 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120806
+# programmers, level0: 두 수의 나눗셈, python3
+def solution(num1: int, num2: int) -> int:
+    return int((num1 / num2) * 1000)
