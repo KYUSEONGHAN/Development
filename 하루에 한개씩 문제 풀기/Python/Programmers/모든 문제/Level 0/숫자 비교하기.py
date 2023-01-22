6cf0485f0c7b35a2a4f083fe5a9e3a547244a903
KYUSEONGHAN <tyty587587@gmail.com>
2023-01-22 23:17:21 +0900

    2023.1.22.일: programmers, level0: 숫자 비교하기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 0/숫자 비교하기.py
@@ -0,0 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120807
+# programmers, level0: 숫자 비교하기, python3
+def solution(num1: int, num2: int) -> int:
+    return 1 if num1 == num2 else -1
