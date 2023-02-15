ed77cc22bd6632539bf97ffa70c4f66321b5a1db
KYUSEONGHAN <tyty587587@gmail.com>
2023-02-15 20:30:01 +0900

    2023.2.15.수: programmers, level0: 모스부호(1), python3

---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 0/모스부호(1).py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120838
+# programmers, level0: 모스부호(1), python3
+def solution(letter: str) -> str:
+    morse = {
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+        '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+        '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+        '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+        '-.--': 'y', '--..': 'z'
+    }
+
+    return ''.join(map(str, [morse[x] for x in letter.split()]))
