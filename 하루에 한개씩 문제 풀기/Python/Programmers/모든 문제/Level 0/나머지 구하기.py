9d2a85d6cddfa8a8aea5883c6ca8d754f9032845
KYUSEONGHAN <tyty587587@gmail.com>
2023-03-01 15:26:59 +0900

    2023.3.1.수: programmers, level0: 나머지 구하기, python3

---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 0/나머지 구하기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120810
+# programmers, level0: 나머지 구하기, python3
+def solution(num1: int, num2: int) -> int:
+    return num1 % num2
+
+if __name__ == '__main__':
+    print(solution(3, 2))   # 1
+    print(solution(10, 5))  # 0
