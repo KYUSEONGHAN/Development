b6ef4a1ad08069b79d762fdb1119d685a210cc95
KYUSEONGHAN <tyty587587@gmail.com>
2023-03-10 13:37:42 +0900

    2023.3.10.금: boj, 24266: 알고리즘 수업 - 알고리즘의 수행 시간 5, python3

---
+++ 하루에 한개씩 문제 풀기/Python/BOJ/시간복잡도/24266.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/24266
+# boj, 24266: 알고리즘 수업 - 알고리즘의 수행 시간 5, python3
+def solve(n: int):
+    print(n * n * n)
+    print(3)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    solve(n)
