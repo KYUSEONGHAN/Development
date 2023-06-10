36028b2337396cbba02c455714d79c5908b34d1a
KYUSEONGHAN <tyty587587@gmail.com>
2023-06-10 22:53:48 +0900

    2023.6.10.토: programmers, level 0: n번째 원소까지

---
+++ 하루에 한개씩 문제 풀기/Go/Programmers/Level 0/n번째 원소까지.go
@@ -0,0 +1,3 @@
+func solution(num_list []int, n int) []int {
+    return num_list[:n]
+}
---
+++ 하루에 한개씩 문제 풀기/Python/Programmers/모든 문제/Level 0/n번째 원소까지.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def solution(num_list: List[int], n: int) -> List[int]:
+    return num_list[:n]
